604fc647811a96bb3f6cc9751e685fae489f042f
KkarliskK <42585445+KkarliskK@users.noreply.github.com>
2024-05-14 18:24:39 +0300

    start of this website

--- frontend/src/view/LandingPage.jsx
+++ frontend/src/view/LandingPage.jsx
@@ -1,9 +1,11 @@
 import '../style/LandingPage.module.css'
+import PeonijaLogo from '../assets/Peonija_izkartne-01.png';
 
 function LandingPage() {
   return (
     <>
-      <div className={`flex w-full bg-black`}>Sveiks lietotāj!</div>
+      <div className={`flex w-full h-auto`}> <img src={PeonijaLogo} /></div>
+      <div className={`flex w-full bg-white`}>Sveiks lietotāj!</div>
     </>
   )
 }
